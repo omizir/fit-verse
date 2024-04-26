12bdc2ef8205b52ec89d9b517a8c64fdf9f99d13
Moritz Brandstätter <arriggon@gmail.com>
2024-04-26 13:47:24 +0200

    add sizes to icon; add icon to user-card

--- src/app/components/ui/icon/icon.component.ts
+++ src/app/components/ui/icon/icon.component.ts
@@ -10,6 +10,18 @@ export class IconComponent {
   iconType = input.required<IconType>();
   stroke = input.required<string>();
   hoverEnabled = input<boolean>(false);
+  size = input<IconSize>('small');
+
+  sizeStrings: { [key in IconSize]: string } = {
+    'small': 'w-6 h-6',
+    'medium': 'w-8 h-8',
+    'large': 'w-12 h-12'
+  };
+
+  getIconSizeString(): string {
+    return this.sizeStrings[this.size()];
+  }
 }
 
 export type IconType = 'trophy' | 'chevron-right' | 'play' | 'chart-bar' | 'cog' | 'user';
+export type IconSize = 'small' | 'medium' | 'large';
